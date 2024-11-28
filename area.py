3a5ca2a3f74dc74249d89da97d1eaea2513e8bd4
Maneesha1306 <156913016+Maneesha1306@users.noreply.github.com>
2024-11-28 14:19:25 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,18 @@
+length=int(input("Enter length of rectangle:"))
+breadth=int(input("Enter breadth of rectangle:"))
+area=length*breadth
+print("Area:",area)
+
+side=int(input("Enter side of square:"))
+area=side*side
+print("Area:",area)
+
+radius=int(input("Enter radius of circle:"))
+area=3.14*radius**2
+print("Area:",area)
+
+base=int(input("Enter base of triangle:"))
+height=int(input("Enter height of triangle:"))
+area=0.5*base*height
+print("Area:",area)
+
